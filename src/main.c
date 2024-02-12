c26248b333d30ba8f66f9e8f5fd495565f24f064
Abdellah <abdellah.belaid.2000@gmail.com>
2024-02-12 11:52:20 +0100

    use the raylib to visualize instead

--- src/main.c
+++ src/main.c
@@ -1,62 +1,30 @@
 #include <stdio.h>
-#include <SDL.h>
+#include <raylib.h>
 
 #include <cpu.h>
 #include <gpu.h>
 #include <memoryUnit.h>
 
-
-SDL_Window* screen;
-SDL_Renderer* render;
+static const int screenWidth = 180;
+static const int screenHeight = 140;
 
 int main(){
+
     int status;
 
     Z80 = initCPU();
     MMU = initMMU();
     GB_GPU = initGPU();
 
-    status = init_window();
-    if(status) return EXIT_FAILURE;
+    InitWindow(screenWidth, screenHeight, "GameBoy Emulator");
+
+    SetTargetFPS(24);
+
+    while(! WindowShouldClose())
+    {
+        // Update the Draw Frame
+    }
     
-    close_window();
-    return EXIT_SUCCESS;
-}
-
-
-int init_window()
-{
-
-    if(!SDL_init(SDL_INIT_VIDEO)){
-        printf("Unable to initialize SDL 2");
-        return EXIT_FAILURE;
-    }
-
-    screen = SDL_CreateWindow("GameBoy", 0, 0, 200, 200, SDL_WINDOW_SHOWN);
-
-    if(screen == NULL){
-        printf("Unable to open Window");
-        return EXIT_FAILURE;
-    }
-
-    render = SDL_CreateRenderer(screen, -1, SDL_RENDERER_SOFTWARE);
-    if(!render){
-        printf("Could not create renderer");
-        return EXIT_FAILURE;
-    }
-
-    SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
-    SDL_RenderClear(render);
-    SDL_RenderPresent(render);
-    SDL_Delay(1000);
-
-    return EXIT_SUCCESS;
-}
-
-int close_window()
-{
-    SDL_DestroyWindow(screen);
-    SDL_Quit();
-
+    CloseWindow();
     return EXIT_SUCCESS;
 }
